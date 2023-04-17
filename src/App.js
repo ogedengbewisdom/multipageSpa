70770ae2525dba8029ea4b6d76ffef7aba90f1da
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-17 15:23:10 +0100

    Defining routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 const router = createBrowserRouter([
   {path: "/", element: <Home />},
-  {}
+  {path: "/products", element: <Products />}
 ]);
 
 function App() {
