6d25b17c1e007b4a391bd31b4f1660de712c295f
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-17 21:50:57 +0100

    Showing Error Pages with errorElement

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import {
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {path: "/", element: <Home />},
       {path: "/products", element: <Products />}
