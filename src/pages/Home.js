2f0bae0b98d66939a8f56826374478b9cab25ba4
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-17 17:11:02 +0100

    using Link instead of anchor tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
+import { Fragment } from "react"
+import { Link } from "react-router-dom"
 
 const Home = () => {
-    return <h1>Welcome to my Home Page</h1>
+    return (
+        <Fragment>
+            <h1>Welcome to my Home Page</h1>
+            <p>Go to <Link to="/products">the list of products</Link></p>
+        </Fragment>
+    )
 }
 
 export default Home
