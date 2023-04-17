221f94d1aa0c59526005cc8eaab249e6b9bb3359
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-17 17:10:08 +0100

    Layout and nested routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import {
+   RouterProvider, 
+   createBrowserRouter, 
+  //  createRoutesFromElements, 
+  //  Route
+   } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
 
-// const router = createBrowserRouter([
-//   {path: "/", element: <Home />},
-//   {path: "/products", element: <Products />}
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {path: "/", element: <Home />},
+      {path: "/products", element: <Products />}
+    ]
+  }
 
-const routeDefinations = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-)
+]);
 
-const router = createBrowserRouter(routeDefinations)
+// const routeDefinations = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routeDefinations)
 
 function App() {
   return <RouterProvider router={router} />
