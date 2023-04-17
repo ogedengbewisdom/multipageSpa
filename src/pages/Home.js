462fe7bdacacced770a493bff86e0569b270d71b
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-17 22:04:16 +0100

    Navigating programatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import { Fragment } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const Home = () => {
+    const navigate = useNavigate()
+
+    const navigateHandler = () => {
+        navigate('/products')
+    }
     return (
         <Fragment>
             <h1>Welcome to my Home Page</h1>
-            <p>Go to <Link to="/products">the list of products</Link></p>
+            <p>Go to <Link to="/products" >the list of products</Link></p>
+            <button onClick={navigateHandler}>onClick</button>
         </Fragment>
     )
 }
