1ba68e822ba8e06be8347139e5b31f0f645e36e9
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-19 03:31:11 +0100

    Adding Links for Dynamic Routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,23 @@
+import { Fragment } from "react"
+import { Link } from "react-router-dom"
+
+
+const DUMMY = [
+    {id: "p1", title: "product 1"},
+    {id: "p2", title: "product 2"},
+    {id: "p3", title: "product 3"}
+]
+
+
 const Products = () => {
-    return <h1>My Products page</h1>
+    return (
+        <Fragment>
+            <h1>My Products page</h1>
+            <ul>
+                {DUMMY.map(item => <li key={item.id}><Link to={`/products/${item.id}`}>{item.title}</Link></li>)}
+            </ul>
+        </Fragment>
+    )
 }
 
 export default Products
