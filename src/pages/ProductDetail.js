ea9711685806b8a4e21e3a61d1730345c050e571
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-19 04:20:34 +0100

    Defining and using Dynamic route

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 
 const ProductDetail = () => {
@@ -8,6 +8,7 @@ const ProductDetail = () => {
         <Fragment>
             <h1>Products Details</h1>
             <p>{param.productId}</p>
+            <button type="button"><Link to=".." relative="path">Back</Link></button>
         </Fragment>
     )
 }
