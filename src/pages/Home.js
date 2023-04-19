850d3a057488fcf61d1a8ee036ad1ab124d6d66d
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-19 04:21:11 +0100

    Navigating programatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,12 @@ const Home = () => {
     const navigate = useNavigate()
 
     const navigateHandler = () => {
-        navigate('/products')
+        navigate('products')
     }
     return (
         <Fragment>
             <h1>Welcome to my Home Page</h1>
-            <p>Go to <Link to="/products" >the list of products</Link></p>
+            <p>Go to <Link to="products" >the list of products</Link></p>
             <button onClick={navigateHandler}>onClick</button>
         </Fragment>
     )
